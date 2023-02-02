3b13d17d7666f8e6a7c31225edbde0f1d3419b16
stasvitalich <stasvitalich1994@gmail.com>
2023-02-03 01:37:58 +0600

    Image of credit card and animation for it have been added.

--- app/src/main/java/com/example/metadata/MainActivity.kt
+++ app/src/main/java/com/example/metadata/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.metadata
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.cardview.widget.CardView
 
 import com.example.metadata.databinding.ActivityMainBinding
 
@@ -15,5 +17,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(s)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val cardView = findViewById<CardView>(R.id.cardView)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.card_animation)
+        cardView.startAnimation(animation)
     }
+
+
+
 }
