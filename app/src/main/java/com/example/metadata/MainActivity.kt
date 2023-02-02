02bec35507d3795d457d890d1ed93d566fdce9a6
stasvitalich <stasvitalich1994@gmail.com>
2023-02-02 21:21:39 +0600

    viewBinding has been added to MainActivity.

--- app/src/main/java/com/example/metadata/MainActivity.kt
+++ app/src/main/java/com/example/metadata/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.metadata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+import com.example.metadata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(s: Bundle?) {
+        super.onCreate(s)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
