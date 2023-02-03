41405de192033affeaf2581b61deb05fd885bcb2
stasvitalich <stasvitalich1994@gmail.com>
2023-02-03 16:37:50 +0600

    Strings were updated. Number container with validation was added to ConstraintLayout.

--- app/src/main/java/com/example/metadata/MainActivity.kt
+++ app/src/main/java/com/example/metadata/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.metadata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.cardview.widget.CardView
 
@@ -17,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(s)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        numberFocusListener()
 
         val cardView = findViewById<CardView>(R.id.cardView)
         val animation = AnimationUtils.loadAnimation(this, R.anim.card_animation)
@@ -24,5 +24,31 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    private fun numberFocusListener() {
+        binding.numberEditText.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.numberContainer.helperText = validNumber()
+            }
+        }
+    }
+
+    private fun validNumber(): String? {
+        val cardNumbers = binding.numberEditText.text.toString()
+
+        if (cardNumbers.isEmpty()) {
+            return getString(R.string.EmptyField)
+        }
+        if (!cardNumbers.matches(".*[0-9].*".toRegex())) {
+            return getString(R.string.OnlyDigitsInCard)
+        }
+
+        if (cardNumbers.length != 8) {
+            return getString(R.string.CardNumberDigit)
+        }
+        if (cardNumbers.matches(".*[*#()/+.,;@!?<>%-].*".toRegex())) {
+            return getString(R.string.CharachtersInNumber)
+        }
+        return null
+    }
 
 }
